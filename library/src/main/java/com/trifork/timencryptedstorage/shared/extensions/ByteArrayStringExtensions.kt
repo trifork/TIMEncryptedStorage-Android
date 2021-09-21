9c72f8f803810de42563cfd070a9c79e79e964ef
Marc le Fevre Johansen <mfj@trifork.com>
2021-09-21 11:06:46 +0200

    Expose string extensions

--- library/src/main/java/com/trifork/timencryptedstorage/shared/extensions/ByteArrayStringExtensions.kt
+++ library/src/main/java/com/trifork/timencryptedstorage/shared/extensions/ByteArrayStringExtensions.kt
@@ -1,7 +1,7 @@
 package com.trifork.timencryptedstorage.shared.extensions
 
-internal val String.asPreservedByteArray: ByteArray
+val String.asPreservedByteArray: ByteArray
     get() = toByteArray(Charsets.ISO_8859_1)
 
-internal val ByteArray.asPreservedString: String
+val ByteArray.asPreservedString: String
     get() = String(this, Charsets.ISO_8859_1)
