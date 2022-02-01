9cd5a665e22d9a6433a067f0874d9326b32c62f7
Jørgen Christian Heinsen <jhe@trifork.com>
2022-02-01 11:35:21 +0100

    Updated publish gradle with groupId and artifactId. Added timKeyServerAwait custom await implementations, so 204 errors codes are correctly error handled.

--- library/src/main/java/com/trifork/timencryptedstorage/keyservice/TIMKeyServiceImpl.kt
+++ library/src/main/java/com/trifork/timencryptedstorage/keyservice/TIMKeyServiceImpl.kt
@@ -1,8 +1,10 @@
 package com.trifork.timencryptedstorage.keyservice
 
 import androidx.annotation.VisibleForTesting
+import com.trifork.timencryptedstorage.BuildConfig
 import com.trifork.timencryptedstorage.models.TIMResult
 import com.trifork.timencryptedstorage.models.errors.TIMKeyServiceError
+import com.trifork.timencryptedstorage.models.errors.TIMKeyServiceErrorCode
 import com.trifork.timencryptedstorage.models.keyservice.TIMKeyServiceConfiguration
 import com.trifork.timencryptedstorage.models.keyservice.TIMKeyServiceVersion
 import com.trifork.timencryptedstorage.models.keyservice.request.TIMKeyRequestBody
@@ -11,9 +13,12 @@ import com.trifork.timencryptedstorage.models.toTIMKeyServiceResult
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
+import kotlinx.coroutines.suspendCancellableCoroutine
 import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.await
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
 class TIMKeyServiceImpl(
@@ -47,7 +52,7 @@ class TIMKeyServiceImpl(
                     keyId,
                     secret
                 )
-            ).await()
+            ).timKeyServerAwait()
         }
     }
 
@@ -63,7 +68,7 @@ class TIMKeyServiceImpl(
                     keyId,
                     longSecret
                 )
-            ).await()
+            ).timKeyServerAwait()
         }
     }
 
@@ -74,12 +79,14 @@ class TIMKeyServiceImpl(
             config: TIMKeyServiceConfiguration
         ): TIMKeyServiceImpl {
 
-            //TODO Separate in to some setup function, this is probably nice to have when developing, but should not be in the production version
-            /*val httpLoggingInterceptor = HttpLoggingInterceptor()
-            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-            val okHttpClient = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()*/
+            var okHttpClient = OkHttpClient.Builder().build()
 
-            val okHttpClient = OkHttpClient.Builder().build()
+            //Is this the correct location for this setup function?
+            if (BuildConfig.BUILD_TYPE.contains("dev")) {
+                val httpLoggingInterceptor = HttpLoggingInterceptor()
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                okHttpClient = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+            }
 
             return instance ?: TIMKeyServiceImpl(
                 api = Retrofit.Builder()
@@ -91,6 +98,39 @@ class TIMKeyServiceImpl(
                 version = config.version
             )
         }
+    }
 
+    // The keyserver gives a 204 error code in case of: KeyLocked
+    // In Retrofit standard await implementation it tries to parse the body of the response, but in case of KeyLocked the keyserver does not return a body
+    // Therefore it would throw a KotlinNullPointerException instead of a HttpException, therefore not trigger the toTIMKeyServiceResult catch block.
+    // We now throw a exception if the error code is 204, so the catch block runs in case of KeyLocked as well
+    private suspend fun <T : Any> Call<T>.timKeyServerAwait(): T {
+        return suspendCancellableCoroutine { continuation ->
+            continuation.invokeOnCancellation {
+                cancel()
+            }
+            enqueue(object : Callback<T> {
+                override fun onResponse(call: Call<T>, response: Response<T>) {
+                    //Here we divert from the Retrofit standard await implementation by disregarding TIMKeyServiceErrorCode.KeyLocked as a successful response code
+                    if (response.code() != TIMKeyServiceErrorCode.KeyLocked && response.isSuccessful) {
+                        val body = response.body()
+                        if (body == null) {
+                            val invocation = call.request().tag(Invocation::class.java)!!
+                            val method = invocation.method()
+                            val e = KotlinNullPointerException("Response from " + method.declaringClass.name + '.' + method.name + " was null but response body type was declared as non-null")
+                            continuation.resumeWithException(e)
+                        } else {
+                            continuation.resume(body)
+                        }
+                    } else {
+                        continuation.resumeWithException(HttpException(response))
+                    }
+                }
+
+                override fun onFailure(call: Call<T>, t: Throwable) {
+                    continuation.resumeWithException(t)
+                }
+            })
+        }
     }
 }
